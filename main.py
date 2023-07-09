169f0c0f3afa0b5045785a753c4b7ec1e2a1b22a
Mateusz Skupien <mateurzskupien@gmail.com>
2023-07-09 22:05:38 +0200

    add: help,join,leave,ping func

--- main.py
+++ main.py
@@ -1,102 +1,54 @@
 import discord
-import asyncio
+import json
 from discord.ext import commands, tasks
-import os
-from dotenv import load_dotenv
-import youtube_dl
 
-load_dotenv()
-DISCORD_TOKEN = os.getenv('MTEyNTAwNDM3MzY3MjU5MTQzMA.GN4gpi.vSVjZgXh8KMx67n8EDE6u2HadNM45MmAJ5gnrc')
 
-intents = discord.Intents().all()
-client = discord.Client(intents=intents)
-bot = commands.Bot(command_prefix='!', intents=intents)
 
-youtube_dl.utils.bug_reports_message = lambda: ''
+intents = discord.Intents().default()
+intents.message_content = True
 
-ytdl_format_options = {
-    'format': 'bestaudio/best',
-    'restrictfilenames': True,
-    'noplaylist': True,
-    'nocheckcertificate': True,
-    'ignoreerrors': False,
-    'logtostderr': False,
-    'quiet': True,
-    'no_warnings': True,
-    'default_search': 'auto',
-    'source_address': '0.0.0.0'
-}
 
-ffmpeg_options = {
-    'options': '-vn'
-}
 
-ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+bot = commands.Bot(command_prefix='!', intents=intents, help_command=None)
 
-class YTDLSource(discord.PCMVolumeTransformer):
-    def __init__(self, source, *, data, volume=0.5):
-        super().__init__(source,volume)
-        self.data = data
-        self.title = data.get('title')
-        self.url = ''
+with open('config.json') as f:
+    data = json.load(f)
+TOKEN = data['BOT_TOKEN']
 
-    @classmethod
-    async def from_url(cls, url, *, loop = None, stream = False):
-        loop = loop or asyncio.get_event_loop()
-        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
-        if 'entries' in data:
-            data = data['entries'][0]
-        filename = data['title'] if stream else ytdl.prepare_filename(data)
-        return filename
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user.name} ({bot.user.id})')
+    channel = bot.get_channel(1125005010690920461)
+    await channel.send('Ready to go')
 
-    @bot.command(name='join')
-    async def join(self,ctx):
-        if not ctx.message.author.voice:
-            await ctx.send('{} is not connected to a voice channel'.format(ctx.message.author.name))
-            return 
-        else:
-            channel = ctx.message.author.voice.channel
-            await channel.connect()
 
-    @bot.command(name='play')
-    async def play(self,ctx,url):
-        server = ctx.message.guild
-        voice_channel = server.voice_client
-        async with ctx.typing():
-            filename = await YTDLSource.from_url(url, loop=bot.loop)
-            voice_channel.play(discord.FFmpegPCMAudio(executable='C:\\Users\\mateu\\OneDrive\\Pulpit\\discordbot\\ffmpeg-2023-06-27-git-9b6d191a66-full_build\\bin', source=filename))
-        await ctx.send('**Now Playing:** {}'.format(filename))
+@bot.command()
+async def join(ctx):
+    try:
+        channel = ctx.author.voice.channel
+        await channel.connect()
+    except AttributeError:
+        await ctx.send("You're currently not on any channel.")
 
-    @bot.command(name='pause')
-    async def pause(self,ctx):
-        voice_client = ctx.message.guild.voice_client
-        if voice_client.is_playing():
-            await voice_client.pause()
-        else:
-            await ctx.send('The bot is paused')
+@bot.command()
+async def leave(ctx):
+    if ctx.voice_client:
+        await ctx.voice_client.disconnect()
+    else:
+        await ctx.send('I am not currently in a voice channel.')
 
-    @bot.command(name='resume')
-    async def resume(self,ctx):
-        voice_client = ctx.message.guild.voice_client
-        if voice_client.is_paused():
-            await voice_client.resume()
-        else:
-            await ctx.send('The bot is already playing')
-    @bot.command(name='leave')
-    async def leave(self,ctx):
-        voice_client = ctx.message.guild.voice_client
-        if voice_client.is_connected():
-            await voice_client.disconnect()
-        else:
-            await ctx.send('Bot has not joined to a voice channel')
+@bot.command()
+async def ping(ctx):
+    await ctx.reply('<:zap:1127665844676206676> Pong! <:zap:1127665844676206676>')
 
-    @bot.command(name='stop')
-    async def stop(self,ctx):
-        voice_client = ctx.message.guild.voice_client
-        if voice_client.is_playing():
-            await voice_client.stop()
-        else:
-            await ctx.message('The bot is not playing any music')
-
-    if __name__ == '__main__':
-        bot.run('MTEyNTAwNDM3MzY3MjU5MTQzMA.GN4gpi.vSVjZgXh8KMx67n8EDE6u2HadNM45MmAJ5gnrc')
+@bot.command()
+async def help(ctx):
+    embed=discord.Embed(title="<:zap:1127665844676206676> Path of Enlightenment: Guidance from Prometheus", description="------", color=0x13aec3)
+    embed.set_author(name="mathie", url="https://github.com/mateuszskuupien", icon_url="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSeNU-w9yItRfskqQcAu83QpLx7vg7jtgUX15Jec7EBCg&s")
+    embed.set_thumbnail(url="https://t4.ftcdn.net/jpg/05/62/26/83/360_F_562268305_PAhZKJquJnGun5zwCc1e2GjfXHG0sAGk.jpg")
+    embed.add_field(name="!join <:zap:1127665844676206676>", value="Summon Prometheus, the celestial companion, and traverse the divine halls of Olympus together.", inline=False)
+    embed.add_field(name="!leave", value="Release your connection to Prometheus, the titan of enlightenment, as you step back into the mortal world.", inline=False)
+    embed.add_field(name="!ping", value=" Challenge your opponents to a mythical game of ping-pong, where lightning-fast reflexes reign supreme.", inline=False)
+    embed.set_footer(text="\"Immortality is not the absence of death, but rather the eternal presence of one's deeds and legacy.\"")
+    await ctx.send(embed=embed)
+bot.run(TOKEN)
